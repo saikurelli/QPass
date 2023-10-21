c3fa9a803dfa7913cc0cabef0049cd7b972fdee6
saikurelli <sairajareddykurelli@gmail.com>
2023-10-21 17:26:11 -0500

    Initial Commit

---
+++ QPass/QPassApp.swift
@@ -0,0 +1,16 @@
+// Project: QPass
+//  
+//  EID: sk49777
+//  Course: CS371L
+
+
+import SwiftUI
+
+@main
+struct QPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
